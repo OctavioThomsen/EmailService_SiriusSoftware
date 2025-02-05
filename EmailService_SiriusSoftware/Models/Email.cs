e8d3365390c5d0aa97b7dc26b3bfc7121ccdfc42
Octavio Thomsen <oti_thomsen98@hotmail.com>
2025-02-05 12:48:36 -0300

    Creación de carpeta raíz models e implementación de modelo email

---
+++ EmailService_SiriusSoftware/Models/Email.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EmailService_SiriusSoftware.Models
+{
+    public class Email
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonPropertyName("id_email")]
+        public int ID_EMAIL { get; set; }
+        public string? SENDER { get; set; }
+        public string? RECIPIENT { get; set; }
+        public string? BODY { get; set; }
+        public string? SEND_STATUS { get; set; }
+    }
+}

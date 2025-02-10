f0f2121abe6adce826b6067f3250a6f7ccdc1939
Octavio Thomsen <otithomsen99@gmail.com>
2025-02-10 00:32:53 -0300

    Remuevo carpeta SQL ya que manejo todo con las migraciones directamente

--- SQL/Script_1_CreateDataBase.sql
+++
@@ -1,5 +0,0 @@
-USE MASTER
-GO
-
-CREATE DATABASE EmailServiceDB
-GO
--- SQL/Script_3_Create_Data_Email.sql
+++
@@ -1,8 +0,0 @@
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'EMAIL') AND type = N'U')
-BEGIN
-    DELETE FROM EMAIL;
-
-    INSERT INTO EMAIL (Sender, Recipient, Body, SendStatus)
-    VALUES ('oti_thomsen98@gmail.com', 'oti_thomsen99@gmail.com', 'Test mail nro 1, ya enviado', 'ENVIADO'),
-           ('oti_thomsen98@gmail.com', 'oti_thomsen99@gmail.com', 'Test mail nro 2, pendiente de enviar', 'PENDIENTE');
-END
